050684d4c89047302d3ba84e0d959b47cda11884
Katie Ok <okdonga@gmail.com>
2015-11-16 11:40:28 -0800

    made working tab

--- js/application.js
+++ js/application.js
@@ -1,20 +1,22 @@
 $(document).ready(function() {
-  console.log('here');
-
   // randomly generate the initial tab to be selected
-  var randomNo = 2;
   // add active class tab
-  var className = 'menu-' + randomNo;
-  ($("." + className)).show()
+  var randomNo = Math.floor(Math.random() * (5) + 1);
+  var contentName = 'menu-' + randomNo;
+  var className = 'menu-' + randomNo + '-tab';
+
+  $("." + className).parent().addClass("selected");
+  ($("." + contentName)).show();
 
   // on click, generate corresponding tab content
   $('nav>ul>li>a').click(function(event) {
     event.preventDefault();
-    // move selected class tab
-    // remove it from previous pos
-    // .addClass('selected')
     $('section>div').css('display', 'none');
+    $("." + className).parent().removeClass('selected');
+
     var selectedContent = $(this).attr('href');
-    ($("." + selectedContent)).show();
+    var selectedClassName = selectedContent + '-tab'
+    $("." + selectedClassName).parent().addClass("selected");
+    $("." + selectedContent).show();
   });
 })
